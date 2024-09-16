55105ad4ca945b4fc08f39dd31251d7ae3f67295
Angel Sharma <angel.dev.cse@gmail.com>
2024-09-16 09:34:06 +0600

    Added db:export to export data and fixed User Role.ts import

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
-import { Roles } from "./role";
+import { Roles } from "./Role";
 import bcrypt from "bcrypt";
 
 interface IUser extends Document {
