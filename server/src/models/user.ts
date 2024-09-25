55eb4fb131c271e4facb1f4c65b8f582d256f64f
Angel <angel.dev.cse@gmail.com>
2024-09-25 12:24:58 +0600

    Removed personal inventory (User should create another organization for storing personal inventory)

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -82,10 +82,6 @@ var userSchema: Schema = new Schema(
         },
       },
     ],
-    inventory: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Inventory"
-    },
     SYSTEM_ADMIN: {
       type: Boolean,
       default: false,
